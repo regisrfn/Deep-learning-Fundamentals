7c04ca9032dd3b6837272760048e63a3365bdd22
Regis Rufino <regis.rfnrodrigues@gmail.com>
2019-02-22 15:53:19 -0300

    Simplification with matricial form

--- gradient_descent.py
+++ gradient_descent.py
@@ -4,27 +4,33 @@ import numpy as np
 def get_cost(x, y, w = 0.5, bias = 0.0):
     
     cost = 0.0
-
     m = x.size
-    for i in range (m):
-        cost += np.square(y[i] - (w*x[i]+bias))
+
+    error = (y-bias) - np.dot(x,w)
+    cost = np.square(error)
+
+    cost = cost.sum(axis=0)
 
     return cost/(2*m)
 
-def adjust_weights(x,y, w = 0.0, b = 0.0, learning_rate = 0.1):
-    size = x.size
-
+def adjust_weights(x,y, w, b=0, learning_rate = 0.01):
+    N = len(x)
+    
     # derivative
-    dw = -x*(y- (w*x+b))/size
-    db = -(y- (w*x+b))/size
+    error = (y-b) - np.dot(x,w)
+    
+    gradient = np.dot(-x.T, error)
+    gradient_bias = -np.sum(error)
 
-    derivative_of_weigth = dw.sum(axis=0)
-    derivative_of_bias = db.sum(axis=0)
+    gradient /= N
+    gradient_bias /= N
 
-    new_weight = w - learning_rate * derivative_of_weigth
-    new_bias = b - learning_rate * derivative_of_bias
+    gradient *= learning_rate
+    gradient_bias *= learning_rate
 
+    weights = w - gradient
+    bias = b - gradient_bias
 
-    return (new_weight, new_bias)
+    return (weights, bias)
 
 
--- main.py
+++ main.py
@@ -5,32 +5,44 @@ import gradient_descent
 # Fixing random state for reproducibility
 # np.random.seed(19680801)
 
-w = 2
-b = 10
-x = np.arange(0.0, 100, 1)
-noise = np.random.normal(1, 10, x.shape)
-y0 = w*x + b
+w = np.array([
+    [2.],
+    [2.]
+])
+
+b = 50
+
+x1 = np.arange(0.0, 10, 1)
+x2 = np.arange(0.0, 10, 1)
+x =  np.array([x1,x2]).T
+
+
+y0 = np.dot(x,w) + b
+noise = np.random.normal(1, 1, y0.shape)
 y = y0 + noise
 
 epochs = 1000
-new_weight = 5.0
+new_weight = np.array([
+    [0],
+    [0]
+])
 new_bias = 1.0
 
-from sklearn import preprocessing
-# normalize the data attributes
-normalized_X = preprocessing.normalize([x])
-normalized_X = normalized_X[0]
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_scaled = sc.fit_transform(x)
+
 
 for epoch in range(epochs):
-    new_weight, new_bias = gradient_descent.adjust_weights(normalized_X,y,new_weight,new_bias)    
+    new_weight, new_bias = gradient_descent.adjust_weights(x_scaled,y,new_weight, new_bias)    
 
-print(new_weight,new_bias)
-print(gradient_descent.get_cost(normalized_X,y,new_weight,new_bias))
+print(gradient_descent.get_cost(x_scaled,y,new_weight,new_bias))
 print(gradient_descent.get_cost(x,y,w,b))
 
-predicted_Y = new_weight*normalized_X + new_bias
+predicted_Y = np.dot(x_scaled,new_weight) + new_bias
 
-plt.scatter(x, y)
-plt.plot(x, y0)
-plt.plot(x, predicted_Y)
+plt.scatter(x1, y)
+plt.plot(x1, y0)
+plt.plot(x1, predicted_Y)
 plt.show()
