84de0fef8937d53e42c2c83b2f9556af6a142dd2
Regis Rufino <regis.rfnrodrigues@gmail.com>
2019-02-26 19:22:35 -0300

    Spliting dataset into test and train

--- main_data.py
+++ main_data.py
@@ -11,6 +11,17 @@ X = dataset.iloc[:, 1:4].values
 Y = dataset.iloc[:, 4].values
 Y = np.array(Y, ndmin=2).T
 
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
 epochs = 1000
 new_weight = np.array([
     [0],
@@ -18,23 +29,14 @@ new_weight = np.array([
     [0]
 ])
 new_bias = 1.0
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-x_scaled = sc.fit_transform(X)
-
-
-
 for epoch in range(1000):
-    new_weight, new_bias = gradient_descent.adjust_weights(x_scaled,Y,new_weight, new_bias)
+    new_weight, new_bias = gradient_descent.adjust_weights(x_train,y_train,new_weight, new_bias)
 
 
 print(new_weight,new_bias)
-print(gradient_descent.get_cost(x_scaled,Y,new_weight,new_bias))
+print(gradient_descent.get_cost(x_test,y_test,new_weight,new_bias))
 
-predicted_Y = np.dot(x_scaled,new_weight) + new_bias
-plt.scatter(X[:, 1], Y)
-# plt.plot(x1, y0)
-plt.plot(X[:, 1], predicted_Y)
-plt.show()
+# predicted_y = np.dot(x_test,new_weight) + new_bias
+# plt.scatter(x_test[:, 1], y_test)
+# plt.plot(x_test[:, 1], predicted_y)
+# plt.show()
