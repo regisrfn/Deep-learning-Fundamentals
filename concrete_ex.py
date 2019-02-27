1849dd349165138658e125230d79ceac834072cc
Regis Rufino <regis.rfnrodrigues@gmail.com>
2019-02-27 15:29:08 -0300

    Linear regression -  another example with a different dataset

---
+++ concrete_ex.py
@@ -0,0 +1,39 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import gradient_descent
+
+# Importing the dataset
+# Importing the dataset
+dataset = pd.read_csv('./concrete_data.csv')
+X = dataset.iloc[:, 0:8].values
+Y = dataset.iloc[:, 8].values
+Y = np.array(Y, ndmin=2).T
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
+epochs = 1000
+new_weight = np.zeros((x_train.shape[1], 1))
+new_bias = 1.0
+
+for epoch in range(1000):
+    new_weight, new_bias = gradient_descent.adjust_weights(x_train,y_train,new_weight, new_bias)
+
+
+# print(new_weight,new_bias)
+print(f'loss:{gradient_descent.get_cost(x_test,y_test,new_weight,new_bias)}')
+
+# predicted_y = np.dot(x_test,new_weight) + new_bias
+# plt.scatter(x_test[:, 1], y_test)
+# plt.plot(x_test[:, 1], predicted_y)
+# plt.show()
